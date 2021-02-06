0c8a740b347b549770707c112edbc8a7f8c6082c
cdamyx <chris@amyx.engineering>
2021-02-05 21:13:44 -0600

    simon complete, please add your customizations

--- game.js
+++ game.js
@@ -19,11 +19,35 @@ function nextSequence() {
 $('.btn').click(function(){
 	var userChosenColor = this.id;
 	userClickedPattern.push(userChosenColor);
+	checkAnswer(userClickedPattern.length - 1);
 	// alert(userChosenColor);
 	// console.log(userClickedPattern);
 	playSound(userChosenColor);
 	animatePress(userChosenColor);
 });
+
+function checkAnswer(currentLevel){
+	if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+		console.log("success");
+		if (userClickedPattern.length === gamePattern.length) {
+			setTimeout(function(){
+				nextSequence();
+			}, 1000);
+			userClickedPattern = [];
+		}
+	}
+	else {
+		console.log("wrong");
+		var wrongAudio = new Audio("sounds/wrong.mp3");
+		wrongAudio.play();
+		$("body").addClass("game-over");
+		setTimeout(function(){
+			$("body").removeClass("game-over");
+		}, 200);
+		$("#level-title").html("Game Over, Press any Key to Restart");
+		startOver();
+	}
+}
 
 function playSound(name) {
 	var audio = new Audio("sounds/" + name + ".mp3");
@@ -45,3 +69,10 @@ $(document).keypress(function() {
 	}
 	
 });
+
+function startOver() {
+	level = 0;
+	gamePattern = [];
+	userClickedPattern = [];
+	started = 0;
+}
